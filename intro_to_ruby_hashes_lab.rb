1e90ddd1726a4f58a284304face9a272177e675e
Jack Goode <jack.w.goode@icloud.com>
2020-06-08 22:57:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
 	monopoly[:railroads][:pieces] = 4
 	monopoly[:railroads][:names] = {}
 	monopoly[:railrods][:rent_in_dollars] = {}
-
+  return monopoly
 end
 
 def monopoly_with_third_tier
